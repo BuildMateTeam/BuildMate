e30a788fd76e4726cb5723e88dedd32631fc845a
BuildMateTeam <buildmatebe@gmail.com>
2026-06-30 18:01:19 +0700

    feat: BuildMate v1.0 Rebuild UI

--- js/parser.js
+++ js/parser.js
@@ -1,1 +1,34 @@
+// =====================================
+// BuildMate v1.0 Rebuild
+// parser.js
+// =====================================
 
+function parseStructure(file){
+
+    return{
+
+        name:file.name,
+
+        size:file.size,
+
+        type:file.type || "Unknown",
+
+        extension:file.name.split(".").pop(),
+
+        loaded:true,
+
+        width:"?",
+
+        height:"?",
+
+        length:"?",
+
+        blocks:"?",
+
+        palette:"?",
+
+        version:"v1.0"
+
+    };
+
+}
